d7c4265a8d0a3f4ea57ba6f182ee1846a207aa1f
gmschz <gmschz92@gmail.com>
2025-04-05 12:07:32 -0600

    Check Bounds logic

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
+// 1. Configs & Constants
 let playerX = 280;
 let playerY = 180;
 const devMode = true;
-const player = document.getElementById("player");
 
+// 2. DOM References
+const player = document.getElementById("player");
+const gamecontainer = document.querySelector(".game-container");
+const containerRect = gamecontainer.getBoundingClientRect();
+
+// 3. Event Listeners
 document.addEventListener("keydown", function(event) {
     //TEMP for debugging
     if (devMode){
         console.log(event.key)
     }
     
-
     // Movement logic here
     if (event.key === "ArrowUp") {
         playerY -= 10;
@@ -20,11 +25,30 @@ document.addEventListener("keydown", function(event) {
     } else if (event.key === "ArrowRight") {
         playerX += 10;
     }
+    checkBounds();
     drawPlayer();
 
     
 })
+
+// 5. Functions (helpers, utilities)
 function drawPlayer() {
     player.style.top = playerY + "px";
     player.style.left = playerX + "px";
+}
+
+
+function checkBounds() {
+    //Player can't go outside the game container
+   playerX = Math.max(0, playerX);
+   playerY = Math.max(0, playerY);
+
+   //Player can't go the right or bottom of the game container
+    const maxX = containerRect.width - player.offsetWidth;
+    const maxY = containerRect.height - player.offsetHeight;
+
+    playerX = Math.min(maxX, playerX);
+    playerY = Math.min(maxY, playerY);
+
+
 }
