3529ad6f826f03107e497190897b7de80e8f5cbd
gmschz <gmschz92@gmail.com>
2025-04-05 11:14:43 -0600

    Styles and Js added

---
+++ script.js
@@ -0,0 +1,26 @@
+let playerX = 280;
+let playerY = 180;
+const devMode = true;
+
+
+document.addEventListener("keydown", function(event) {
+    //TEMP for debugging
+    if (devMode){
+        console.log(event.key)
+    }
+    
+
+    // Movement logic here
+    if (event.key === "ArrowUp") {
+        playerY -= 10;
+    } else if (event.key === "ArrowDown") {
+        playerY += 10;
+    } else if (event.key === "ArrowLeft") {
+        playerX -= 10;
+    } else if (event.key === "ArrowRight") {
+        playerX += 10;
+    }
+    drawPlayer();
+
+    
+})
